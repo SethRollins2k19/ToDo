5de6fdc10ddc2b7b074b2a01d06a7fe2dbc3c59e
SethRollins2k19 <46203101+SethRollins2k19@users.noreply.github.com>
2020-02-25 00:53:50 +0300

    Updates

---
+++ precache-manifest.83f36aaa7f84592a18b0e0f6d05367a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0dbc4b01fc92cdcba19e938b9db140",
+    "url": "/SethRollins2k19/ToDo.git/index.html"
+  },
+  {
+    "revision": "dce4f5be3f637c88b448",
+    "url": "/SethRollins2k19/ToDo.git/static/css/main.8ac506ad.chunk.css"
+  },
+  {
+    "revision": "a685c4da8cd96ad34142",
+    "url": "/SethRollins2k19/ToDo.git/static/js/2.579f9201.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/SethRollins2k19/ToDo.git/static/js/2.579f9201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce4f5be3f637c88b448",
+    "url": "/SethRollins2k19/ToDo.git/static/js/main.37ee41d3.chunk.js"
+  },
+  {
+    "revision": "2919fcba7e4d84dba4af",
+    "url": "/SethRollins2k19/ToDo.git/static/js/runtime-main.add648a6.js"
+  }
+]);
